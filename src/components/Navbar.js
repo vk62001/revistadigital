0c180e49511a3b00f3dd38bce60b3b66e007c839
FreYserSantiago <oliverafreyser@gmail.com>
2021-10-28 20:34:25 -0500

    na

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,43 @@ import { Link, useLocation } from 'react-router-dom';
 const NavBar = props => {
 
     return (
-        <div className="h-1/4 flex w-full">
-            <div className="desktop:w-1/2 laptop:w-1/2 tablet:w-0 phone:w-full">
-                <p className="desktop:hidden laptop:hidden tablet:hidden sm:block text-white">
-                    <i className="fas fa-bars"></i>
+        <div className="h-1/4 flex w-full px-2 desktop:px-0">
+            <div className="desktop:w-1/2 laptop:w-1/2 tablet:w-0 phone:w-full cursor-pointer">
+                <p className="desktop:hidden laptop:hidden tablet:hidden phone:block text-white
+                 flex">
+                <i className="fas fa-bars"></i>
                 </p>
             </div>
-            <div className="desktop:w-1/2 tablet:w-full tablet:block phone:hidden">
+            <div id="menu" className="desktop:w-1/2 tablet:w-full tablet:block phone:hidden ">
                 <ul className="flex justify-center items-center">
                 <Link to={"/" }  className={'p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white flex'}>
                         <i className="fas fa-home"></i>
                         <p className="m-0 text-xs ml-2">Inicio</p>
                 </Link>
-                <Link to={"/contact" }  className={'p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white'}>
+                <Link to={"/login" }  className={'p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white flex'}>
                         <i className="fas fa-user"></i>
-                        <p className="m-0 text-xs">Contacto</p>
+                        <p className="m-0 text-xs"></p>
                 </Link>
-                <li className="p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white " >Iniciar Sesión</li>
-                <li className="p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white ">Ediciones</li>
-                <li className="p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white ">Sobre la revista</li>
-                <li className="p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white ">Comentarios</li>
-                <li className="p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white ">Contacto</li>
+                <Link to={"/editions"} className={"p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white flex"}>
+                     <i className="fas fa-book-open"></i>
+                     <p className="m-0 text-xs ml-1">Ediciones</p>
+                     </Link>
+                     <Link to={"/about"} className={"p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white flex "}>
+                     <i className="fas fa-bookmark"></i>
+                     <p className="m-0 text-xs ml-1 ">Sobre la Revista</p>
+                     </Link>
+                     <Link to={"/comments"} className={"p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white flex"}>
+                     <i className="fas fa-comment-dots"></i>
+                     <p className="m-0 text-xs ml-1">Comentarios</p>
+                     </Link>
+                     <Link to={"/contact"} className={"p-4 text-white cursor-pointer hover:border-white hover:border-b-2 border__white flex "}>
+                     <i className="fas fa-address-card"></i>
+                     <p className="m-0 text-xs ml-1">Contacto</p>
+                     </Link>
+                
+    
+                
+                
                 </ul>
             </div>
         </div>
