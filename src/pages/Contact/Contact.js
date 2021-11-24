77e7c4fcef5c6841978d9d9e8e9e80546d21129c
eder <eder@iMac-de-eder.local>
2021-11-24 14:01:37 -0600

    fix contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -9,55 +9,79 @@ const Contact = () => {
                  <NavBar 
                     white={false}
                  />
-                
+                <div className="
+                                p-10
+                                phone:flex-none phone:p-4
+                                tablet:flex
+                                laptop:flex
+                                desktop:flex
+                ">
+                    <div className="
+                        contácto 
+                        phone:w-full phone:flex-none phone:absolute phone:bottom-0 phone:justify-start phone:text-xs
+                        tablet:relative tablet:w-1/2 tablet:flex tablet:justify-center tablet:items-center
+                        laptop:relative laptop:w-1/2 laptop:flex laptop:justify-center laptop:items-center
+                        desktop:relative desktop:w-1/2 desktop:flex desktop:justify-center desktop:items-center
+                        "
+                    >
+                        <div className="float-left">
+                            <div className="icon-text mb-1 flex justify-center items-center text-blue-700">
+                                 <i className="fas fa-map-marker-alt p-2"></i>
+                                 <div className="">
+                                    <p> Carretera Ixtaltepec -Comitancillo Km. 7.5</p> 
+                                    <p> San Pedro Comitancillo</p>
+                                 </div>
+                                
+                            </div>
+                            <div className="icon-text mb-1 text-blue-700">
+                                <span><i className="far fa-envelope p-1" aria-hidden="true"></i> <a href="mailto:cc_comitancillo@tecnm.mx">cc_comitancillo@tecnm.mx</a></span>
+                            </div>
+                            <div className="icon-text mb-1 flex justify-start items-center text-blue-700">
+                                 <i className="fas fa-mobile-alt p-2"></i>
+                                 <div className="">
+                                    <p>(01 971) 717 21 09 </p>
+                                    <p>(01 971) 717 21 10</p>
+                                 </div>
+                                
+                            </div>
+                            <div className="icon-text mb-1 text-blue-700">
+                                <i class="fab fa-facebook-square cursor-pointer text-3xl"></i> <i class="fab fa-twitter-square cursor-pointer text-3xl"></i> <i class="fab fa-instagram-square cursor-pointer text-3xl"></i> <i class="fab fa-whatsapp-square cursor-pointer text-3xl"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form 
+                                    phone:w-full
+                                    tablet:w-1/2
+                                    laptop:w-1/2
+                                    destkop:w-1/2
+                                    "
+                    >
+                            <form className="
+                                    phone:py-7 phone:px-5 phone:bg-gray-100 phone:rounded-xl
+                                    tablet:w-full tablet:py-20 tablet:px-5 tablet:bg-gray-100 tablet:rounded-xl tablet:shadow-2xl
+                                    laptop:w-full laptop:py-20 laptpop:px-5 laptop:bg-gray-100 laptop:rounded-xl laptop:shadow-2xl
+                                    desktop:w-8/12 desktop:py-20 laptpop:px-5 desktop:bg-gray-100 desktop:rounded-xl desktop:shadow-2xl desktop:float-left
+                                     ">
+                                <h2 className="text-3xl text-center mb-5 text-blue-900 font-sans font-bold uppercase">Contacto</h2>
+                                <input className="w-full rounded mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600" type="text" placeholder="Ingrese su nombre"/>
+                                <input className="w-full mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600" type="email" placeholder="Ingrese su correo"/>
+                                <input className="w-full mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600"type= "text" placeholder="Asunto"s/>
+                                <input className="w-full mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600"type= "text" placeholder="Mensaje"/>
+                                <button className="rounded py-2.5  px-10 mt-10 border-none text-sm bg-blue-900 font-semibold cursor-pointer text-white outline-none hover:bg-blue-500" type="button">
+                                    Enviar
+                                </button>
+                            </form>
+                    </div>  
+                </div>
                 
                 <div className="absolute top-0 right-0 mt-5 mr-5">
-                <img className="w-40
+                    <img className="w-40
                                 phone:hidden
                                 tablet:hidden tablet:w-1/4 tablet:fixed tablet:left-6
                                 laptop:block laptop:w-1/8 laptop:fixed laptop:right-6
                                 desktop: block desktop:w-1/6 desktop:fixed desktop:right-0" src={sep} alt="sep"/>
                 </div>
-                        <div className="flex items-center w-full max-w-sm absolute top-80 right-24">
-                            <form className="w-full py-20 px-5 bg-gray-100 absolute rounded-3xl shadow-2xl
-                            laptop:block laptop:w-full laptop:right-2 laptop:my-2
-                            desktop:block desktop:w-full desktop:right-16 desktop:my-8
-                            tablet:block tablet:w-full tablet:-left-8 tablet:my-4
-                            phone:block phone:w-11/12  phone:left-28">
-                                <h2 className="text-3xl text-center mb-5 text-blue-900 font-sans font-bold uppercase">Contacto</h2>
-                                <input className="w-full mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600" type="text" placeholder="Ingrese su nombre"/>
-                                <input className="w-full mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600" type="email" placeholder="Ingrese su correo"/>
-                                <input className="w-full mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600"type= "text" placeholder="Asunto"s/>
-                                <input className="w-full mt-5 p-2.5 border-none bg-white text-base outline-none focus:ring-2 focus:ring-blue-600"type= "text" placeholder="Mensaje"/>
-                                <button className="py-2.5  px-10 mt-10 border-none text-sm bg-blue-900 font-semibold cursor-pointer text-white outline-none hover:bg-blue-500" type="button">
-                                    Enviar
-                                </button>
-                            </form>
-                        </div>
-                    
-                        <div className="absolute left-0 mt-32 ml-52
-                        phone:block phone:w-1/2 phone:-bottom-56 phone:left-0 phone:ml-20
-                        tablet:block tablet:w-1/2 tablet:-bottom-26 tablet:left-20 tablet:ml-20
-                        laptop:block laptop:w-1/2 laptop:left-0 laptop:bottom-20
-                        desktop:block desktop:w-1/2 desktop:left-20 desktop:my-0">
-                        <div className="icon-text">
-                        <i className="fas fa-map-marker-alt"></i> <span>Carretera Ixtaltepec -Comitancillo Km. 7.5 <br/>
-                        San Pedro Comitancillo</span>
-                        </div>
-                        <br/>
-                        <br/>
-                        <div className="icon-text">
-                        <i className="far fa-envelope" aria-hidden="true"></i> <span>cc_comitancillo@tecnm.mx</span>
-                        </div>
-                        <br/>
-                        <br/>
-                        <div className="icon-text">
-                        <i className="fas fa-mobile-alt"></i> <span>(01 971) 717 21 09 <br/> (01 971) 717 21 10</span>
-                        <br/>
-                        <br/>
-                        <i class="fab fa-facebook-square cursor-pointer text-3xl"></i> <i class="fab fa-twitter-square cursor-pointer text-3xl"></i> <i class="fab fa-instagram-square cursor-pointer text-3xl"></i> <i class="fab fa-whatsapp-square cursor-pointer text-3xl"></i>
-                        </div>
-                    </div>        
+                         
         </div>
     )
 }
