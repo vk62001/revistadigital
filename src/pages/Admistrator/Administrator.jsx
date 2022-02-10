2a35ce56eba5ea5e729b4a8e562fe82ec4433097
FreYserSantiago <oliverafreyser@gmail.com>
2022-02-09 22:17:46 -0600

    administrator

--- src/pages/Admistrator/Administrator.jsx
+++ src/pages/Admistrator/Administrator.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
+import {IconButton, Tooltip, Button} from '@material-ui/core';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import AttactchFileIcon from '@material-ui/icons/AttachFile';
+import axios from 'axios';
 
+const initialValues = {
+  archivo: null,
+  archivoNombre: "",
+  archivoURL:""
+}
 const Administrator = () => {
+
+  const [archivo, setArchivo] = useState(initialValues);
+
+  const fileSelectHandler = (e) => {
+    setArchivo({
+      archivo: e.target.files[0],
+      archivoNombre: e.target.files[0].name
+    })
+  }
+
+  const onSubmit = e =>{
+
+    e.preventDefault();
+    const fd = new FormData();
+    fd.append('file', archivo.archivo, archivo.archivoNombre);
+    axios.post("http://localhost:8000/api/upload", fd,{
+      onUploadProgress: progressEvent => {
+        console.log('Upload progress: ' + Math.round(progressEvent.loaded / progressEvent.total * 100)+ "%");
+
+
+      } 
+    })
+    
+  }
+
   return <div>
-      <h1>Administrador</h1>
-      <label>Favor de seleccionar el archivo a subir</label>
-      <input type="file" />
+      <h1>Administrador Revista Digital</h1>
+      <input id ="archivo-input" type="file" onChange={fileSelectHandler} />
+      <label htmlFor='archivo-input'> 
+        <Tooltip title="Adjuntar un archivo">
+          <IconButton color="primary" component="span">
+          <AttactchFileIcon fontSize="large"/>
+          </IconButton>
+        </Tooltip>
+      </label>
+      <Button 
+       variant="contained" onClick={onSubmit}>
+         Aceptar
+         </Button>
+      
   </div>;
 };
 
